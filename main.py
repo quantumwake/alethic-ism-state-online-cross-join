68902dbeda882457b06822b86fd3481d384c5ac8
Kasra Rasaee <krasaee@gmail.com>
2024-07-29 19:59:15 -0700

    not sure, somethign small probably

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import asyncio
 import os
+import random
 from typing import List
 
 import dotenv
@@ -205,4 +206,5 @@ if __name__ == '__main__':
     )
 
     consumer.setup_shutdown_signal()
-    asyncio.get_event_loop().run_until_complete(consumer.start_consumer())
+    consumer_no = random.randint(0, 10)
+    asyncio.get_event_loop().run_until_complete(consumer.start_consumer(consumer_no=consumer_no))
