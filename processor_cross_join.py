fd3fa124c4b164bed1239bbd8b415aa54ff2584b
Kasra Rasaee <krasaee@gmail.com>
2025-06-21 11:32:59 -0700

    fix process_input_data function entrypoint for processor name change

--- processor_cross_join.py
+++ processor_cross_join.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import dotenv
 from ismcore.model.processor_state import extract_values_from_query_state_by_key_definition, State
 from ismcore.processor.base_processor import BaseProcessor
@@ -19,8 +21,9 @@ class OnlineCrossJoinProcessor(BaseProcessor):
         self.primary_input_state = primary_input_state
         self.secondary_input_state = secondary_input_state
 
-    async def process_input_data_entry(self, input_query_state: dict, force: bool = False):
+    # async def process_input_data_entry(self, input_query_state: dict, force: bool = False):
 
+    async def process_input_data(self, input_data: dict | List[dict], force: bool = False):
         # merges two sets together and ensures it always takes the value that is `not` None.
         def merge_dicts(primary, secondary):
             return {
@@ -37,9 +40,9 @@ class OnlineCrossJoinProcessor(BaseProcessor):
         if self.config.query_state_inheritance:
             primary_query_state = extract_values_from_query_state_by_key_definition(
                 key_definitions=self.config.query_state_inheritance,
-                query_state=input_query_state)
+                query_state=input_data)
         else:
-            primary_query_state = input_query_state
+            primary_query_state = input_data
 
         output_query_states = []
         for secondary_index in range(secondary_count):
